963cad5831780f6146461e4495c20b81dcd55455
allan <aejepsen@yahoo.com.br>
2023-02-01 13:07:16 -0300

    ok

--- styledComponents/my-app/src/App.js
+++ styledComponents/my-app/src/App.js
@@ -5,11 +5,17 @@ const Titulo = styled.h1`
 color: red;
 `;
 
+const Versiculo = styled.p`
+color: white;
+`;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Titulo>Deus é Fiel</Titulo>
+        <Versiculo>Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna.
+João 3:16</Versiculo>
       </header>
     </div>
   );
