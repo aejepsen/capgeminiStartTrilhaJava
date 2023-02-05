1c97ddda6f606be563e7855c675196e405a07e9e
allan <aejepsen@yahoo.com.br>
2023-02-05 17:56:55 -0300

    javascript

---
+++ JavaScript/01-Script.js
@@ -0,0 +1,21 @@
+
+
+// var nome = "Glória a Deus!";
+// var idade = 18;
+// var altura = 1.80;
+// var verdadeiro = true;
+// var falso = false;
+
+// window.alert(nome);
+// window.alert(idade);
+// window.alert(altura);
+
+var cursos = [
+
+  {nome: "HTML", cargaHoraria: 20, categoria: "Básico"},
+  {nome: "CSS", cargaHoraria: 20, categoria: "Básico"},
+  {nome: "JavaScript", cargaHoraria: 40, categoria: "Intermediário"},
+];
+
+window.alert(cursos[0].nome);
+window.alert(cursos[1].nome+ " - " +cursos[1].cargaHoraria);
