f68c18044eab0f6feed794fbbcd828ec69f754ff
allan <aejepsen@yahoo.com.br>
2023-02-08 18:25:12 -0300

    componente-personalizado

--- Angular/first-angular/src/app/app.module.ts
+++ Angular/first-angular/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PipeMultiplicaPorComponent } from './pipe-multiplica-por/pipe-multiplic
 import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
 import { FormsModule } from '@angular/forms';
 import { RenderizandoListasComponent } from './renderizando-listas/renderizando-listas.component';
+import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { RenderizandoListasComponent } from './renderizando-listas/renderizando-
     MultiplicaPorPipe,
     PipeMultiplicaPorComponent,
     TwoWayDataBindingComponent,
-    RenderizandoListasComponent
+    RenderizandoListasComponent,
+    ComponentePersonalizadoComponent
   ],
   imports: [
     BrowserModule,
