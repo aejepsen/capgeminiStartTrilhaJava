67843bf1c299ecff856130fead4f28768d42602e
allan <aejepsen@yahoo.com.br>
2023-02-25 14:48:13 -0300

    fava fundamentos

--- javaFundamentos/TiposDeDados/src/TipoDado.java
+++ javaFundamentos/TiposDeDados/src/TipoDado.java
@@ -3,18 +3,18 @@ public class TipoDado {
 
 	public static void main(String[] args) {
 		
-		// demonstração de alguns tipos de dados
-		int     numero;
-		float   decimalCurto;
-		double  decimalLongo;
-		char    caracter;
-		String  cadeiaCaracteres;
+		// demonstraï¿½ï¿½o de alguns tipos de dados
+		int     numero = 10;
+		float   decimalCurto = 10.5f;
+		double  decimalLongo = 20.5d;
+		char    caracter = 'a';
+		String  cadeiaCaracteres= "** Meu Curso de Java Fundamentos em 2021 **";
 		
-		numero       = 10;
-		decimalCurto = 10.5f;
-		decimalLongo = 20.5;
-		caracter     = 'A';
-		cadeiaCaracteres = "** Meu Curso de Java Fundamentos em 2021 **";
+		System.out.println("numero = " + numero);
+		System.out.println("decimalCurto = " + decimalCurto);
+		System.out.println("decimalLongo = " + decimalLongo);
+		System.out.println("caracter = " + caracter);
+		System.out.println("cadeiaCaracteres = " + cadeiaCaracteres);
 	}
 
 }
