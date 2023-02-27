3694b8ad959843f04f307dd2e9fe01eec3a6355c
allan <aejepsen@yahoo.com.br>
2023-02-27 11:13:56 -0300

    acentuação

--- linguagemProgramacaoJava/Materiais/1-Construtores-Enumerador-Interfaces/src/Construtores/Construtor.java
+++ linguagemProgramacaoJava/Materiais/1-Construtores-Enumerador-Interfaces/src/Construtores/Construtor.java
@@ -5,19 +5,19 @@ import Construtores.Aluno.STATUS;
 public class Construtor {
 
 	public static void main(String[] args) {
-		// passando os dados através do construtor com sobrecarga
+		// passando os dados atravÃ©s do construtor com sobrecarga
 		Aluno alu = new Aluno( 8,  8,  8);
-		// calculando a média
+		// calculando a mÃ©dia
 		double mediaAlunoFinal = alu.calcularMediaAluno();
 		
-		// definindo situação do aluno
+		// definindo situaÃ§Ã£o do aluno
 		if (mediaAlunoFinal < 6 ) {
 			alu.situacaoAluno = STATUS.REPROVADO;
 		} else {
 			alu.situacaoAluno = STATUS.APROVADO;
 		}
 		
-		// imprimindo situação do aluno
-		System.out.println("A média do aluno é : " + alu.calcularMediaAluno() + " e o aluno está " + alu.situacaoAluno);
+		// imprimindo situaÃ§Ã£o do aluno
+		System.out.println("A mÃ©dia do aluno Ã© : " + alu.calcularMediaAluno() + " e o aluno estÃ¡ " + alu.situacaoAluno);
 	}
 }
